e460033695253fb1db548eb5f45faf9a2db8eb97
John Cifone <john.c6485@gmail.com>
2024-07-17 01:07:12 -0400

    adding movie graphs

--- frontend/src/graphs.js
+++ frontend/src/graphs.js
@@ -5,16 +5,16 @@ import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Toolti
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  labels: ['1920-1920', '1930-1939', '1940-1949', '1950-1959', '1960-1969', '1970-1979', '1980-1989', '1990-1999', '2000-2009', '2010-2019', '2020-2029'],
   datasets: [
     {
-      label: 'My First dataset',
+      label: 'Distribution by Decade',
       backgroundColor: 'rgba(75,192,192,1)',
       borderColor: 'rgba(75,192,192,1)',
       borderWidth: 1,
       hoverBackgroundColor: 'rgba(75,192,192,0.4)',
       hoverBorderColor: 'rgba(75,192,192,1)',
-      data: [65, 59, 80, 81, 56, 55, 40],
+      data: [65, 59, 80, 81, 56, 55, 40, 12, 16, 54, 2],
     },
   ],
 };
@@ -27,15 +27,14 @@ const options = {
     },
     title: {
       display: true,
-      text: 'Chart.js Bar Chart Example',
+      text: 'Distribution by Decade',
     },
   },
 };
 
 export default function Graph() {
   return (
-    <div>
-      <h2>Bar Example (custom size)</h2>
+    <div className="barGraph">
       <Bar data={data} options={options} />
     </div>
   );
